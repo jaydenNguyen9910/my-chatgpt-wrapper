3e60a6d2e0dc9da5f307e29e21114e4a84880224
Nguyen Minh Tri <nguyenminhtri11101999@gmail.com>
2023-03-22 00:24:01 +0700

    Add server IP to Django allowed hosts

--- chatgpt_server/chatgpt_server/settings.py
+++ chatgpt_server/chatgpt_server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*#1-=lsp063b(l4gx0i*xox==puxv&65wvgp8rgc2q4a)8qw$!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'localhost', '113.160.226.174']
 
 # Application definition
 
